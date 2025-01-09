24553ecfb78737aefe5065e60d91a270acfe1d6f
artick788 <60552965+artick788@users.noreply.github.com>
2025-01-09 17:41:43 +0100

    Added basic logger

--- include/SyriusUtils/Logger/LoggerMacros.hpp
+++ include/SyriusUtils/Logger/LoggerMacros.hpp
@@ -6,3 +6,8 @@
 #define SR_LOG_WARNING(source, message) Syrius::Logger::logWarning(source, message, SR_FUNC, SR_FILE, SR_LINE)
 #define SR_LOG_ERROR(source, message) Syrius::Logger::logError(source, message, SR_FUNC, SR_FILE, SR_LINE)
 #define SR_LOG_THROW(source, message) Syrius::Logger::logThrow(source, message, SR_FUNC, SR_FILE, SR_LINE)
+
+#define SR_LOG_INFO_IF_FALSE(condition, source, message) if (!(condition)) { SR_LOG_INFO(source, message); }
+#define SR_LOG_WARN_IF_FALSE(condition, source, message) if (!(condition)) { SR_LOG_WARNING(source, message); }
+#define SR_LOG_ERROR_IF_FALSE(condition, source, message) if (!(condition)) { SR_LOG_ERROR(source, message); }
+#define SR_LOG_THROW_IF_FALSE(condition, source, message) if (!(condition)) { SR_LOG_THROW(source, message); }
