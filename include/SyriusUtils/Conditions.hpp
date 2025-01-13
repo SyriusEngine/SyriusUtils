48ab9600f7ecee2c06130537f08315ea77cdbc25
artick788 <60552965+artick788@users.noreply.github.com>
2025-01-13 20:46:48 +0100

    Fixed macros and assertion functions when a string with no formatting is provided

--- include/SyriusUtils/Conditions.hpp
+++ include/SyriusUtils/Conditions.hpp
@@ -9,23 +9,30 @@
 namespace Syrius::Priv {
 
     template<typename... Args>
-    void formatAssert(const char* condition, const char* file, const char* func, int line, char const* const message, Args... args){
-        std::size_t size = snprintf(nullptr, 0, message, args...);
-        std::vector<char> buffer(size + 1);
-        snprintf(buffer.data(), size + 1, message, args...);
+    void formatAssert(const char* assertionType, const char* condition, const char* file, const char* func, int line, char const* const message, Args... args){
+        if constexpr (sizeof...(args) == 0){
+            printf("[%s]: [%s : %s : %i]: Condition: %s failed\n", assertionType, file, func, line, condition);
+            fflush(stdout); // print message BEFORE program halts
+            std::abort();
+        }
+        else{
+            std::size_t size = snprintf(nullptr, 0, message, args...);
+            std::vector<char> buffer(size + 1);
+            snprintf(buffer.data(), size + 1, message, args...);
 
-        printf("[ASSERT]: [%s : %s : %i]: Condition: %s failed: %s\n", file, func, line, condition, buffer.data());
-        fflush(stdout); // print message BEFORE program halts
-        std::abort();
+            printf("[%s]: [%s : %s : %i]: Condition: %s failed: %s\n", assertionType, file, func, line, condition, buffer.data());
+            fflush(stdout); // print message BEFORE program halts
+            std::abort();
+        }
     }
 }
 
 #if defined(SR_DEBUG)
 
-    #define SR_ASSERT(condition, msg, ...) Syrius::Priv::formatAssert(#condition, SR_FILE, SR_FUNC, SR_LINE, msg, ##__VA_ARGS__);
+    #define SR_ASSERT(condition, msg, ...) if (!(condition)) Syrius::Priv::formatAssert("ASSERT", #condition, SR_FILE, SR_FUNC, SR_LINE, msg, ##__VA_ARGS__);
 
-    #define SR_PRECONDITION(condition, msg, ...) SR_ASSERT(condition, msg, ##__VA_ARGS__)
-    #define SR_POSTCONDITION(condition, msg, ...) SR_ASSERT(condition, msg, ##__VA_ARGS__)
+    #define SR_PRECONDITION(condition, msg, ...) if (!(condition)) Syrius::Priv::formatAssert("PRECONDITION", #condition, SR_FILE, SR_FUNC, SR_LINE, msg, ##__VA_ARGS__);
+    #define SR_POSTCONDITION(condition, msg, ...) if (!(condition)) Syrius::Priv::formatAssert("POSTCONDITION", #condition, SR_FILE, SR_FUNC, SR_LINE, msg, ##__VA_ARGS__);
 #else
     #define SR_ASSERT(condition, msg, ...)
     #define SR_PRECONDITION(condition, msg, ...)
--- include/SyriusUtils/Logger/Logger.hpp
+++ include/SyriusUtils/Logger/Logger.hpp
@@ -18,15 +18,20 @@ namespace Syrius{
         template<typename... Args>
         static void log(SR_MESSAGE_SEVERITY severity, const std::string& source, const std::string& message,
                         const std::string& function, const std::string& file, u32 line, Args... args){
-            std::size_t size = snprintf(nullptr, 0, message.c_str(), args...);
-            std::vector<char> buffer(size + 1);
-            snprintf(buffer.data(), size + 1, message.c_str(), args...);
-            std::string formattedMessage(buffer.data());
-
             Message msg;
+            if constexpr (sizeof...(args) != 0 ){
+                std::size_t size = snprintf(nullptr, 0, message.c_str(), args...);
+                std::vector<char> buffer(size + 1);
+                snprintf(buffer.data(), size + 1, message.c_str(), args...);
+                std::string formattedMessage(buffer.data());
+
+                msg.message = formattedMessage;
+            }
+            else{
+                msg.message = message;
+            }
             msg.severity = severity;
             msg.source = source;
-            msg.message = formattedMessage;
             msg.function = function;
             msg.file = file;
             msg.line = line;
